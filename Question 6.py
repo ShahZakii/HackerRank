df9ffbd885049aed96326f7acc961756f44b3b05
ShahZakii <shahhzaki@gmail.com>
2026-02-01 21:49:03 +0530

    Added HackerRank Python Question.

---
+++ Question 6.py
@@ -0,0 +1,16 @@
+# HackerRank Python Question.
+
+# Question 5:
+# The included code stub will read an integer,n, from STDIN.
+# Without using any string methods, try to print the following:
+# 123...n
+# Note that "..." represents the consecutive values in between.
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    newstr = ""
+    for i in range(1,n+1):
+        type = str(i)
+        newstr += type
+    print(newstr)
