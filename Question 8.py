49dd670dd90cb610b3b6bb60fff4f15e5441c145
ShahZakii <shahhzaki@gmail.com>
2026-02-04 19:23:39 +0530

    Correct mistake

--- Question 8.py
+++ Question 8.py
@@ -1,6 +1,6 @@
 # HackerRank Python Question.
 
-# Question 5:
+# Question 8:
 # Given the participants' score sheet for your University Sports Day, you are required to find the 
 # runner-up score. You are given n scores. Store them in a list and find the score of the runner-up.
 
