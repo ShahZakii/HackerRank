3321bc025c12427204a90f075d41c5c307af318d
ShahZakii <shahhzaki@gmail.com>
2026-02-05 22:06:36 +0530

    Added HackerRank Question.

--- Question 6.py
+++ Question 6.py
@@ -1,6 +1,6 @@
 # HackerRank Python Question.
 
-# Question 5:
+# Question 6:
 # The included code stub will read an integer,n, from STDIN.
 # Without using any string methods, try to print the following:
 # 123...n
--- Question 7.py
+++ Question 7.py
@@ -1,6 +1,6 @@
 # HackerRank Python Question.
 
-# Question 5:
+# Question 7:
 # Let's learn about list comprehensions! You are given three integers x,y and z representing the dimensions 
 # of a cuboid along with an integer n. Print a list of all possible coordinates given by (i,j,k) on a 3D grid where the 
 # sum of i+j+k is not equal to n.
